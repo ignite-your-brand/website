46cd85cfccf8bedeaa24319028f53c63509196b8
Nemanja Tisma <tisma@vitamin2.ch>
2018-03-26 22:47:29 +0200

    Small fix

--- app/models/jobs.js
+++ app/models/jobs.js
@@ -1,6 +1,5 @@
 import Contentful from 'ember-data-contentful/models/contentful';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
 
 export default Contentful.extend({
   name: attr('string'),
